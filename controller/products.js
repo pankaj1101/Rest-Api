f48d4af7a97d33ae515b67da3e1c433ace671927
pankaj <pankajram1101@gmail.com>
2023-08-20 13:00:27 +0530

    Done

--- controller/products.js
+++ controller/products.js
@@ -4,7 +4,7 @@ const Product = require('../models/products');
 
 const getAllProucts = async (req, res) => {
 
-    const myData = await Product.find();
+    const myData = await Product.find({});
     res.status(200).json({ myData });
 
 }
